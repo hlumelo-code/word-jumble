ac778d250a9903979f037e664c62a6784623ed4e
Hlumelo William <hlumelowilliam@proton.me>
2026-05-08 17:48:22 +0200

    while loop in main, time sleeps for introduction texts

--- src/wordjumble/main.py
+++ src/wordjumble/main.py
@@ -2,32 +2,41 @@ from utils import * # importing functionss from the helpers file
 import os
 
 def main(file):
-    
-    clearTerminal()
-
-    tprint("WORD JUMBLE")
-    # introduction / welcoming the player.
-    print("                                           \033[35m" + "WELCOME" + "\033[0m")
-    print("In this game, you are given a JUMBLED word - you have to \033[34mUNJUMBLE\033[0m it to move to the next round, and eventually levels.\n"
-          "To UNJUMBLE a word is to reorder the given characters to match the sequence of characters of the hidden word.\n"
-          "Example, for the word DOG, jumbled -> \033[36m[D G O]\033[0m. You would have to reorder these characters correctly into \033[36mD O G\033[0m to win the round.\n")
-    
-    print("\nPress SPACE or ENTER to start, press 'Q' to quit the game.")
-
+   
     try:
-        k = readkey()
-        if (k == key.SPACE or k == key.ENTER):
-            clearTerminal()
-        elif (k == 'q'):
-            clearTerminal()
-            sys.exit(0)
+        clearTerminal()
 
-        level_words = getWords(file)
-        # testing game by interating through the words... this is not the final logic of the program.
-        for word in level_words:
+        tprint("WORD JUMBLE")
+        time.sleep(0.3)
+        # introduction / welcoming the player.
+        print("                                           \033[35m" + "WELCOME" + "\033[0m")
+        print("In this game, you are given a JUMBLED word - you have to \033[34mUNJUMBLE\033[0m it to move to the next round, and eventually levels.\n"
+              "To UNJUMBLE a word is to reorder the given characters to match the sequence of characters of the hidden word.\n"
+              "Example, for the word DOG, jumbled -> \033[36m[D G O]\033[0m. You would have to reorder these characters correctly into \033[36mD O G\033[0m to win the round.\n")
+        time.sleep(0.3)
+        print("\nPress SPACE or ENTER to start, press 'Q' to quit the game.")
 
-            word = word.lower()
-            play(word)
+        while True:
+
+            k = readkey()
+            if (k == key.SPACE or k == key.ENTER): # chooses to play
+                
+                level_words = getWords(file)
+
+                for word in level_words:
+
+                    word = word.lower()
+                    play(word)
+
+            elif (k == 'q'): # chooses to quit
+                clearTerminal()
+                sys.exit(0)
+
+            else: # invalid input
+                print("INVALID INPUT", end="", flush=True)
+                time.sleep(1)
+                print("\r" + " " * 30, end="\r", flush=True) 
+                continue
 
     except(KeyboardInterrupt):
         clearTerminal()
